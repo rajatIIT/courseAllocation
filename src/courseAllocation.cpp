c71d5a87979a0756fb57734f3e038240fdc178be
Rajat Pawar <rajatpawar@hotmail.com>
2016-02-29 18:06:20 -0500

    start the project

---
+++ src/courseAllocation.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : courseAllocation.cpp
+// Author      : Rajat Pawar
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
