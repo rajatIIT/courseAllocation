58822fd4d31609228c9dd3ebf0e22ba033271ee5
Rajat Pawar <rajatpawar@hotmail.com>
2016-02-29 18:12:41 -0500

    add institution

---
+++ src/Institution.cpp
@@ -0,0 +1,21 @@
+/*
+ * Institution.cpp
+ *
+ *  Created on: Feb 18, 2016
+ *      Author: rajatpawar
+ */
+
+#include "Institution.h"
+
+namespace survey {
+
+Institution::Institution() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Institution::~Institution() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace survey */
---
+++ src/Institution.h
@@ -0,0 +1,21 @@
+/*
+ * Institution.h
+ *
+ *  Created on: Feb 18, 2016
+ *      Author: rajatpawar
+ */
+
+#ifndef INSTITUTION_H_
+#define INSTITUTION_H_
+
+namespace survey {
+
+class Institution {
+public:
+	Institution();
+	virtual ~Institution();
+};
+
+} /* namespace survey */
+
+#endif /* INSTITUTION_H_ */
