2e6f64e9cc5e875fd1c520bfcdd62370f7a92b8b
Rajat Pawar <rajatpawar@hotmail.com>
2016-02-29 18:11:45 -0500

    add course

---
+++ src/Course.cpp
@@ -0,0 +1,21 @@
+/*
+ * Course.cpp
+ *
+ *  Created on: Feb 18, 2016
+ *      Author: rajatpawar
+ */
+
+#include "Course.h"
+
+namespace survey {
+
+Course::Course() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Course::~Course() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace survey */
---
+++ src/Course.h
@@ -0,0 +1,22 @@
+/*
+ * Course.h
+ *
+ *  Created on: Feb 18, 2016
+ *      Author: rajatpawar
+ */
+
+#ifndef COURSE_H_
+#define COURSE_H_
+
+namespace survey {
+
+class Course {
+public:
+	Course();
+	int courseID;
+	virtual ~Course();
+};
+
+} /* namespace survey */
+
+#endif /* COURSE_H_ */
